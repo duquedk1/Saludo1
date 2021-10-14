fbb4bf0c313b44c5f1b8953c6eea2ef6dc02e406
Daniel <duquedk1@gmail.com>
2021-10-14 17:25:10 -0500

    inicial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`Hola ${req.query.nombre} !`);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
