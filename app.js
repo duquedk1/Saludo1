aad5aedfa25e2fcab330aa4a526d454e4d6755ef
Daniel <duquedk1@gmail.com>
2021-10-15 09:33:53 -0500

    feat: add req.query.nombre= 'desconocido'

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 const app = express();
 
+
 app.get("/", (req, res) => {
-  res.send(`Hola ${req.query.nombre} !`);
+
+  req.query.nombre = "desconocido"
+  res.send(`Hola ${req.query.nombre}!`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
