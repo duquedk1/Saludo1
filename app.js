78dad04cb709e0ea4ee3c14815a28375ab68caf8
Daniel <duquedk1@gmail.com>
2021-10-15 09:49:32 -0500

    refactor: app.get

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import express from "express";
 const app = express();
-
-
-app.get("/", (req, res) => {
-
-  req.query.nombre = "desconocido"
-  res.send(`Hola ${req.query.nombre}!`);
-});
+app.get("/", function (req, res) {
+    req.query.nombre = "desconocido";
+    res.send(`<h1>Hola ${req.query.nombre}!</h1>`);
+  });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
