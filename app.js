4e0baddee26277efe125519e3c99f1c173f6bd5c
Daniel Duque <82002029+duquedk1@users.noreply.github.com>
2021-10-16 21:12:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 const app = express();
 app.get("/", function (req, res) {
-    req.query.nombre = "desconocido";
-    res.send(`<h1>Hola ${req.query.nombre}!</h1>`);
+   const { nombre } = req.query;
+  if(!nombre) res.send(`<h1>Hola desconocido!</h1>`);
+  else {
+      res.send(`<h1>Hola ${nombre}!</h1>`);
+  }
   });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
